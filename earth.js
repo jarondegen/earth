16ee89b6c608db49be6de5cce0aff6436d1f147e
Jaron Degen <jaron.degen90@gmail.com>
2020-07-01 02:50:30 +0200

    The Final day

--- earth.js
+++ earth.js
@@ -2,15 +2,9 @@
   "name": "Earth",
   "populations": [
     "dinosaurs": 0,
-<<<<<<< HEAD
     "asteroids": 3,
     "mammoths": 35,
     "humans": 0,
-    "aliens": 700
-=======
-    "asteroids": 0,
-    "mammoths": 0,
-    "humans": 100+,
->>>>>>> master
+    "aliens": 701
   ]
 }
